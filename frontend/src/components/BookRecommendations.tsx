9bbf5935b3967c680c8d406de8ec8049a1e8b15d
mrdoing <35158665+mrdoing@users.noreply.github.com>
2023-12-13 17:18:36 +0800

    Update BookRecommendations.tsx

--- frontend/src/components/BookRecommendations.tsx
+++ frontend/src/components/BookRecommendations.tsx
@@ -14,8 +14,7 @@ const BookRecommendations: React.FC = () => {
 
   const fetchBooksForCategory = async (category: string, offset = 0) => {
     try {
-      const categoryWithoutEmoji = removeEmoji(category);
-      const response = await search({ query: categoryWithoutEmoji, limit: pageSize, offset });
+      const response = await search({ query: category, limit: pageSize, offset });
       const newBooks = response.books || [];
 
       // 随机排序
@@ -44,7 +43,6 @@ const BookRecommendations: React.FC = () => {
 
   const emojis = ['📚', '🧠', '🎨', '✏️', '📖', '🤔', '📜', '📚', '📜', '⛪️', '💻', '💰', '🏛️', '⚔️', '🧒'];
 
-  const removeEmoji = (text: string): string => text.replace(/[\u{1F600}-\u{1F6FF}]/gu, '');
 
   return (
     <Flex direction={{ base: 'row', md: 'column' }} px={{ base: 0, md: 4 }} py={0} align="center" mt={4} mb={4}>
